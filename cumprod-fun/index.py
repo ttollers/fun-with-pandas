1c29837b49a621910ae4009b090a7c0a6966273a
Chris Norman <chrisnorman1988@gmail.com>
2017-08-02 20:28:33 +0100

    cumprod functionality

--- cumprod-fun/index.py
+++ cumprod-fun/index.py
@@ -1,6 +1,10 @@
 
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
 
-df = pandas.read_csv("./data.cdv")
-print(df)
+df = pandas.read_csv("./data.csv")
+
+df['CumProd'] = np.cumprod(df['point_latitude'])
+
+df_florida = df[df['statecode']=='FL']
+df_Clay = df[df['county']== 'CLAY COUNTY']
