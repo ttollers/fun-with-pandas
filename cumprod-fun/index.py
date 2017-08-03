b7f29b0c35a810d854c669bfe3c2ab117f6f06be
Chris Norman <chrisnorman1988@gmail.com>
2017-08-03 11:50:24 +0100

    regression. No cum prod

--- cumprod-fun/index.py
+++ cumprod-fun/index.py
@@ -6,5 +6,29 @@ df = pandas.read_csv("./data.csv")
 
 df['CumProd'] = np.cumprod(df['point_latitude'])
 
-df_florida = df[df['statecode']=='FL']
-df_Clay = df[df['county']== 'CLAY COUNTY']
+#df_Clay = df[df['county']== 'CLAY COUNTY']
+##df_Clay = df_Clay[df_Clay['construction']== 'Wood']
+#def filterFunction(x):
+#    print(x)
+#    return x['county'] == 'CLAY COUNTY'
+#    
+#
+#
+#df_Smodger = [{
+#        'county': 'CLAY COUNTY'
+#        }, {
+#                'county': 'NOT CLAY COUNTY'
+#                }]
+#
+#df_10inch = list(df['county'])
+#
+#print(list(df.values.tolist()))
+#
+#df_George = list(filter(filterFunction, df.values.tolist()))
+#print(df_George)
+
+import statsmodels.formula.api as sm
+
+result = sm.ols(formula="tiv_2012 ~ tiv_2011 ", data=df).fit()
+
+print(result.summary())
